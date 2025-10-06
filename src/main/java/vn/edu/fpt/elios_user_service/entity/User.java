3cf5b9877cb8551e2fcb30c663951876c9081bc9
Huy Nguyen <huynnthe176346@fpt.edu.vn>
2025-10-07 01:29:16 +0700

    feat: create entity User.java

---
+++ src/main/java/vn/edu/fpt/elios_user_service/entity/User.java
@@ -0,0 +1,41 @@
+package vn.edu.fpt.elios_user_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "uuid")
+    private UUID id;
+
+    @Column(name = "first_name", length = 20, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", length = 20, nullable = false)
+    private String lastName;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "timestamptz")
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false, columnDefinition = "timestamptz")
+    private Instant updatedAt;
+}
