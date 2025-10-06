a49530d2e6bc3f3e6e7ea2dc9d4e122286daf383
Huy Nguyen <huynnthe176346@fpt.edu.vn>
2025-10-06 19:19:49 +0700

    initial commit

---
+++ src/test/java/vn/edu/fpt/elios_user_service/EliosUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt.elios_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliosUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
